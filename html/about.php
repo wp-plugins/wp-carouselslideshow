3e63b00dfa15002af520bdc595c82aeb1bff1c33
wpslideshow <wpslideshow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-16 12:38:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-carouselslideshow/trunk@519798 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/about.php
+++ html/about.php
@@ -4,12 +4,23 @@
 	Carousel slideshow is a flash wordpress slideshow plugin. Using this plugin it is possible to create categories and upload images. This is very easy to use plugin. Visit <a href="http://wpslideshow.com">wpslideshow.com</a> to learn more.
 	</p>
 
-	<h2><?php _e('short codes'); ?></h2>
+	<h2><?php _e('short codes to use in content'); ?></h2>
 	<p>[carousel] - This short code will display all images under all categories which are not disabled.</p>
 
 	<p>[carousel cats=2,3] - This short code will display all images under the categories with ID's 2,3.</p>
 
 	<p>[carousel imgs=1,2,3] - This short code will display images which has ID's 1,2,3.</p>
+
+	<h2><?php _e('short codes to use in template php files'); ?></h2>
+
+	<p> &lt;?php echo do_shortcode('[carousel]');?&gt; – This short code will display all images under all categories which are not disabled.</p>
+
+	<p> &lt;?php echo do_shortcode('[carousel cats=2,3]');?&gt; – This short code will display all images under the categories with ID’s 2,3.</p>
+
+	<p> &lt;?php echo do_shortcode('[carousel imgs=1,2,3]');?&gt; – This short code will display images which has ID’s 1,2,3.</p>
+
+
+
 
 	<h2><?php _e('Carousel Slideshow Installation Video'); ?></h2>
 	<p>
