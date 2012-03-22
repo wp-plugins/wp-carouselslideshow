6187ee7e2630f9e9bb9dab1607fea78811bc96aa
wpslideshow <wpslideshow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-22 09:13:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-carouselslideshow/trunk@522114 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/about.php
+++ html/about.php
@@ -13,11 +13,11 @@
 
 	<h2><?php _e('short codes to use in template php files'); ?></h2>
 
-	<p> &lt;?php echo do_shortcode('[carousel]');?&gt; – This short code will display all images under all categories which are not disabled.</p>
+	<p> &lt;?php echo do_shortcode('[carousel]');?&gt; - This short code will display all images under all categories which are not disabled.</p>
 
-	<p> &lt;?php echo do_shortcode('[carousel cats=2,3]');?&gt; – This short code will display all images under the categories with ID’s 2,3.</p>
+	<p> &lt;?php echo do_shortcode('[carousel cats=2,3]');?&gt; - This short code will display all images under the categories with ID's 2,3.</p>
 
-	<p> &lt;?php echo do_shortcode('[carousel imgs=1,2,3]');?&gt; – This short code will display images which has ID’s 1,2,3.</p>
+	<p> &lt;?php echo do_shortcode('[carousel imgs=1,2,3]');?&gt; - This short code will display images which has ID's 1,2,3.</p>
 
 
 
@@ -25,7 +25,7 @@
 	<h2><?php _e('Carousel Slideshow Installation Video'); ?></h2>
 	<p>
 	<h3>WordPress Carousel slideshow Installation Video</h3>
-		<iframe width="425" height="349" src="http://www.youtube.com/embed/rp78cra5noA?hl=en&fs=1" frameborder="0" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="http://www.youtube.com/embed/wTrnNunHd74" frameborder="0" allowfullscreen></iframe>
 	</p>
 
 </div>
